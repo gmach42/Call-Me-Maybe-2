df4ec87f7ade16a4ecc5240a88237acc3603683b
gmach <gmach@student.42lyon.fr>
2026-05-04 20:57:05 +0200

    created simple llm command to test out token manipulation

--- src/__main__.py
+++ src/__main__.py
@@ -1,23 +1,27 @@
 from llm_sdk.llm_sdk import Small_LLM_Model
-from .demo_llm import run_demo
 
 
 def main() -> None:
-    # model = Small_LLM_Model()
+    model = Small_LLM_Model()
 
-    # prompt = "What is the sum of 2 and 3?"
-    # inputs_ids = model.encode(prompt)[0].tolist()
+    prompt = input("Enter a prompt: ")
+    token_ids = model.encode(prompt)[0].tolist()
 
     # logits = model.get_logits_from_input_ids(inputs_ids)
     # next_token_id = max(range(len(logits)), key=lambda i: logits[i])
 
     # generated_ids = inputs_ids + [next_token_id]
-    # text = model.decode(generated_ids)
 
-    # print("Prompt:", prompt)
-    # print("Next token id:", next_token_id)
-    # print("Generated text:", text)
-    run_demo()
+    for _ in range(50):
+        logits = model.get_logits_from_input_ids(token_ids)
+        next_token_id = max(range(len(logits)), key=lambda i: logits[i])
+        token_ids.append(next_token_id)
+
+    text = model.decode(token_ids)
+
+    print("Prompt:", prompt)
+    print("Next token id:", next_token_id)
+    print("Generated text:", text)
 
 
 if __name__ == "__main__":
